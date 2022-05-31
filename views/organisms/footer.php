baa959073a9d0f69fc92fb5b0fc4d4cd99c00041
LeviTas2003 <levitas2003@gmail.com>
2022-05-31 14:16:28 +0200

    Footer toegevoegd en css aangepast, zo nog merge

--- views/organisms/footer.php
+++ views/organisms/footer.php
@@ -1,3 +1,5 @@
 <footer>
-    FOOTER
+    <div class="footer-side">WonderReview &copy;</div>
+    <div class="footer-middle">"Wonder Review! Wonderbaarlijk goede Reviews!"</div>
+    <a href="<?php echo $url . "/wp-admin" ?>" class="login footer-side">Login</a>
 </footer>
