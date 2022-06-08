cdfa13cee4912ace053ac23e41140ab747949a98
Quindows <q.blume@outlook.com>
2022-06-08 14:56:08 +0200

    fixed archive page

--- archive-reviews.php
+++ archive-reviews.php
@@ -1,22 +1,31 @@
 <?php
-
 $archive = get_post_type_archive_link('reviews');
 
-$arguments = ['post_type' => 'reviews', 'numberposts' => -1, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
+$arguments = ['post_type' => 'reviews', 'numberposts' =>-1, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
 $reviewsItemCollection = get_posts($arguments);
 
-foreach($reviewsItemCollection as $review){
-    // var_dump(get_permalink($review));
-    var_dump($review);
-        
-    ?>
-    <div>
-        <div>
-            <img src=<?php echo get_field('afbeelding', $review->ID) ?> alt="">
+?>
+    <div class="row-archive ">
+    <?php foreach($reviewsItemCollection as $review): ?>
+        <?php
+        // var_dump(get_permalink($review));
+        $url = get_field('link', $review->ID);
+
+        $img = get_field('afbeelding' , $review->ID);
+        $img = $img['url'];
+        ?>
+        <div class="card">
+            <div class="img">
+                <img src="<?php echo $img; ?>" alt="image wont load">
+            </div>
+            <h1><?php echo $review->post_title; ?></h1>
+            <p class="inhoud"><?php echo get_field('inhoud', $review->ID) ?></p>
+            <p class="score">score: <?php echo get_field('score', $review->ID) ?> / 10</p> 
+            <div class="link">
+                <a href="<?php echo $review->post_title; ?>">View review</a>
+            </div>
         </div>
-        <div>
-            <h1>test</h1>
-            <p></p>
-            <p></p>
-        </div>
+    <?php endforeach; ?>
     </div>
+</div>
+
--- views/organisms/footer.php
+++ views/organisms/footer.php
@@ -1,6 +1,6 @@
 
-    <footer>
-        <div class="footer-side">WonderReview &copy;</div>
-        <div class="footer-middle">"Wonder Review! Wonderbaarlijk goede Reviews!"</div>
-        <a href="<?php echo $url . "/wp-admin" ?>" class="login footer-side">Login</a>
-    </footer>
+<footer>
+    <div class="footer-side">WonderReview &copy;</div>
+    <div class="footer-middle">"Wonder Review! Wonderbaarlijk goede Reviews!"</div>
+    <a href="<?php echo $url . "/wp-admin" ?>" class="login footer-side">Login</a>
+</footer>
